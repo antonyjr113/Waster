0c248eaae07b56fcb4072c4a89b90c96ec6febf9
antonyjr113 <antonyjr113@gmail.com>
2024-12-02 21:14:46 +0300

    новая верстка баннером со свитчером и кнопкой

--- Waster/Objects/Banner.swift
+++ Waster/Objects/Banner.swift
@@ -9,61 +9,105 @@ import SwiftUI
 
 struct Banner: View {
     
+    @State private var isTrial = false
+    
     var body: some View {
         
         ZStack {
-            Color.green
+            Color.yellow
                 .ignoresSafeArea()
+            
             VStack {
+                HStack(alignment: .center) {
+                    Text("Try Pro Version!")
+                        .font(.system(size: 34))
+                }
+
                 HStack {
-                    Text("Feature 1")
+                    Text("Feature 1 - helps you do 1st thing")
+                        .padding()
+                    
+                    Spacer()
+                    
+                    Image(systemName: "star.fill")
+                        .padding()
+                }
+                .padding()
+                HStack {
+                    Text("Feature 2 - helps you do 2nd thing")
                         .padding(.leading, 30)
                     
                     Spacer()
                     
-                    Text("Feature 2")
+                    Image(systemName: "star.fill")
                         .padding(.trailing, 30)
                 }
-                .padding()
                 HStack {
-                    Image(systemName: "star.fill")
-                        .padding(.leading, 55)
+                    Text("Feature 3 - helps you do 3rd thing")
+                        .padding()
                     
                     Spacer()
+                    
                     Image(systemName: "star.fill")
-                        .padding(.trailing, 55)
-                }
-            }
-            .padding(.top, -350)
-            
-            Spacer()
-            
-            HStack(alignment: .center) {
-                Text("Try Pro Version!")
-                    .font(.system(size: 34))
-            }
-        
-            Spacer()
-            
-            VStack {
-                HStack {
-                    Image(systemName: "star.fill")
-                        .padding(.leading, 55)
-                    Spacer()
-                    Image(systemName: "star.fill")
-                        .padding(.trailing, 55)
+                        .padding()
                 }
                 .padding()
+                
                 HStack {
-                    Text("Feature 3")
-                        .padding(.leading, 50)
+                    Text("Feature 4 - helps you do 4th thing")
+                        .padding(.leading, 30)
+                    
                     Spacer()
-                    Text("Feature 4")
-                        .padding(.trailing, 50)
+                    
+                    Image(systemName: "star.fill")
+                        .padding(.trailing, 30)
                 }
             }
-            .padding(.top, 500)
+            .padding(.bottom, 350)
             
+            VStack {
+
+                Toggle(isOn: $isTrial) {
+                    Text("Trial available")
+                        .font(.headline)
+                        
+                }
+                .padding()
+                .border(.blue, width: 3)
+                
+                HStack(alignment: .center) {
+                    Text("Week")
+                        .font(.headline)
+                        .padding()
+                    Spacer()
+                    Text("7.99$")
+                        .font(.headline)
+                        .padding()
+                }
+                .border(.blue, width: 3)
+                
+
+                HStack {
+                    Text("Year")
+                        .font(.headline)
+                        .padding()
+                    Spacer()
+                    Text("39.99$")
+                        .font(.headline)
+                        .padding(.trailing)
+                }
+                .border(.blue, width: 3)
+
+            }
+
+            .padding(.top, 300)
+            
+            Button("Continue"){
+                
+            }
+            .buttonStyle(.borderedProminent)
+            .font(Font.title)
+            .padding(.top, 650)
 
         }
     }
