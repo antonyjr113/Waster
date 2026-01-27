91ca92c59394b504217413ecb6b610865326c3b7
Anton Beloborodov <a.beloborodov@aiby.com>
2026-01-27 23:14:09 +0300

    new files for user profile, wastes data model, UD keys, currency values - for maintanance

---
+++ Waster/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Cyrrency.swift
+//  Waster
+//
+//  Created by Don Wolfton on 27.01.26.
+//
+
+import Foundation
+
+enum Currency: String {
+    case dollar = "dollar"
+    case ruble = "ruble"
+    case euro = "euro"
+}
---
+++ Waster/UserDefualtsKeys.swift
@@ -0,0 +1,15 @@
+//
+//  UserDefualtsKeys.swift
+//  Waster
+//
+//  Created by Don Wolfton on 27.01.26.
+//
+
+import Foundation
+
+enum UserDefualtsKeys: String {
+    case wastesData = "wastesData"
+    case monthlyReport = "monthlyReport"
+    case customPeriodReport = "customPeriodReport"
+    case userProfile = "userProfile"
+}
