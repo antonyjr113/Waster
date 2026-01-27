db3a01daaf5262359ca467e57e43fb0395b15b91
Anton Beloborodov <a.beloborodov@aiby.com>
2026-01-27 23:14:09 +0300

    new files for user profile, wastes data model, UD keys, currency values - for maintanance

---
+++ Waster/UserProfileConfig.swift
@@ -0,0 +1,15 @@
+//
+//  UserProfileConfig.swift
+//  Waster
+//
+//  Created by Don Wolfton on 27.01.26.
+//
+
+import UIKit
+
+struct UserProfileConfig {
+    var name: Int
+    var avatar: String
+    var currency: Currency
+    var budjet: Int
+}
---
+++ Waster/WastesDataModel.swift
@@ -0,0 +1,20 @@
+//
+//  WastesDataModel.swift
+//  Waster
+//
+//  Created by Don Wolfton on 27.01.26.
+//
+
+import UIKit
+import SwiftUI
+
+final class WastesDataModel {
+    var allWastes: [Waste] = []
+    
+    private func deleteWastes() {
+        
+    }
+    private func sortWastes(type: Int) {
+        
+    }
+}
